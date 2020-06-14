830f72c478b4319a23c02c0b4a8a8bb2d1473066
matthras <matthew.y.p.mack@gmail.com>
2020-06-14 19:55:00 +1000

    reduced number of points to make it solvable in Concorde

--- sample-commands.py
+++ sample-commands.py
@@ -3,6 +3,6 @@ import os
 import sys
 cmd = sys.executable
 
-os.system(cmd + " " + "weighted-voronoi-stippler/stippler.py sample-images/figure.png --save --n_point 2048 --n_iter 25 --pointsize 1.0 1.0 --figsize 6 --threshold 255 --force --interactive --png")
+os.system(cmd + " " + "weighted-voronoi-stippler/stippler.py sample-images/figure.png --save --n_point 512 --n_iter 25 --pointsize 1.0 1.0 --figsize 6 --threshold 255 --force --interactive --png")
 
 # os.system(cmd + " " + "weighted-voronoi-stippler/stippler.py sample-images/figure.png --save --n_point 2048 --n_iter 50 --pointsize 1.0 1.0 --figsize 6 --threshold 255 --force --interactive --png")
