254bdee792ce5a3db6ef5e551dca07a7f7795b79
matthras <matthew.y.p.mack@gmail.com>
2020-06-14 19:54:41 +1000

    added functioning 'drawing the TSP path' script

---
+++ draw.py
@@ -0,0 +1,31 @@
+from PIL import Image, ImageDraw
+
+ORIGINAL_IMAGE = "sample-images/figure.png"
+IMAGE_TSP = "sample-images/figure-stipple.tsp"
+IMAGE_CYC = "sample-images/figure-tour.cyc"
+
+list_of_nodes = []
+
+with open(IMAGE_TSP) as f:
+  for _ in range(7):
+    next(f)
+  for line in f:
+    i,x,y = line.split()
+    list_of_nodes.append((int(float(x)),int(float(y))))
+
+tsp_path = []
+
+with open(IMAGE_CYC) as g:
+  for line in g:
+    tsp_path.append(list_of_nodes[int(line)])
+tsp_path.append(list_of_nodes[0])
+
+original_image = Image.open(ORIGINAL_IMAGE)
+width, height = original_image.size
+
+tsp_image = Image.new("RGBA",(width,height),color='white')
+tsp_image_draw = ImageDraw.Draw(tsp_image)
+#tsp_image_draw.point(tsp_path,fill='black')
+tsp_image_draw.line(tsp_path,fill='black',width=1)
+tsp_image = tsp_image.transpose(Image.FLIP_TOP_BOTTOM)
+tsp_image.save("sample-images/figure-tsp.png")
